d4ccd88ef38032f74600be5aaff688b815ae3bba
BorizZebr <borizzebr@gmail.com>
2016-05-28 02:10:01 +0700

    fixing tests

--- test/DbIntegrationSpec.scala
+++ test/DbIntegrationSpec.scala
@@ -1,13 +1,9 @@
 import org.scalatestplus.play.PlaySpec
-import play.api.db.Databases
 
 /**
   * Created by borisbondarenko on 26.05.16.
   */
 class DbIntegrationSpec extends PlaySpec {
 
-  Databases.withInMemory(){ database =>
 
-
-  }
 }
